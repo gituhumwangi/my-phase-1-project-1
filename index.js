a41b052fca1fe9abdd5a964b40c623f76e00755f
dennis <dennis.mwangi@student.moringaschool.com>
2023-06-18 11:37:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+//creation of fuction chechMarks 
+function checkMarks (marks) {
+    if (marks > 79) {
+        console.log("A")
+    }
+
+    else if (marks > 60 && marks < 79) {
+        console.log("B-")
+    }
+
+    else if (marks > 59 && marks < 49) {
+        console.log("C-")
+    }
+
+    else if (marks > 49 && marks < 40) {
+        console.log("D-")
+    }
+
+    else if (marks < 40) {
+        console.log("E")
+    }
+}
